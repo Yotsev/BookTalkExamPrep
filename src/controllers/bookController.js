f7f762c6f93b5fb5d8faac8206c769cb499ebcfb
Yotsev <b.yotsev@gmail.com>
2023-02-11 14:25:35 +0200

    Added wishlist functionality

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -33,11 +33,62 @@ bookRouter.post('/create', isAuthenticated, async (req, res) => {
     try {
         await bookService.createBook(book);
     } catch (err) {
-        console.log(err);
         return res.status(400).render('book/create', { error: getErrorMessage(err), book });
     }
 
     res.redirect('/book/catalog');
+});
+
+//Get Details Page
+bookRouter.get('/:id/details', async(req, res)=> {
+    let isWished, isAuthor;
+    const isRegistered = req.user;
+    
+    const book = await bookService.getBookById(req.params.id);
+
+    if (!book) {
+        return res.redirect(404, '/404');
+    }
+
+    if (isRegistered) {
+        isAuthor = book.owner._id == req.user._id;
+        isWished = book.wishList.includes(req.params._id);
+    }
+
+    res.render('book/details', {book, isRegistered, isAuthor, isWished });
+});
+
+//Get Wishlist Page TODO: the wish filed to be removed
+bookRouter.get('/:id/wishlist', async (req, res)=> {
+    const book = await bookService.getBookById(req.params.id);
+    const user = req.user;
+
+    if (!book) {
+        return res.redirect('/404');
+    }
+
+    if (!user) {
+        return res.redirect('/404');
+    }
+    
+    try {
+        if (book.owner._id == req.user._id) {
+            throw new Error(`Can't withlist your own book`);
+        } 
+        
+        await bookService.wishlistBook(book._id, user._id);
+
+        res.locals.isWished = true
+
+        res.redirect(`/book/${book._id}/details`);
+    } catch (err) {
+        console.log(err);
+        return res.status(400).render(`book/details`, {error: getErrorMessage(err), book});
+    }
+});
+
+bookRouter.get('/:id/ewdit', async (req, res)=>{
+
 });
 
 module.exports = bookRouter;
--- src/services/bookService.js
+++ src/services/bookService.js
@@ -1,3 +1,4 @@
+const { findById } = require('../models/Book');
 const Book = require('../models/Book');
 
 exports.createBook = async (book) => {
@@ -7,6 +8,23 @@ exports.createBook = async (book) => {
 exports.getAllBooks = async ()=>{
     return await Book.find({}).lean();
 };
+
+exports.getBookById = async (id)=>{
+    return await Book.findById(id).populate('owner').lean();
+};
+
+exports.wishlistBook = async (bookId, user_id) => {
+    const book = await Book.findById(bookId);
+    
+    if (book.wishList.includes(user_id)) {
+        throw new Error(`Can't wishlist twice`);
+    }
+
+    book.wishList.push(user_id);
+    await book.save();
+};
+
+
 
 //Or like that
 // async function getAll(){
