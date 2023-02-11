c1b7849a39c7700609975a5810f23ff0cdbcbe57
Yotsev <b.yotsev@gmail.com>
2023-02-11 09:07:33 +0200

    Added dinamyc navbar

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,11 @@
 const homeRouter = require('express').Router();
 
 homeRouter.get('/', (req, res)=> {
-    res.render('home');
+    res.render('home/home');
 });
+
+homeRouter.get('/404', (req, res)=> {
+    res.render('home/404')
+})
 
 module.exports = homeRouter;
