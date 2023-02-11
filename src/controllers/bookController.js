b5055c65c10c1d81c6e72d9a8b2e49223fdd0361
Yotsev <b.yotsev@gmail.com>
2023-02-11 12:07:42 +0200

    Added dynamic catalog

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -5,8 +5,12 @@ const bookService = require('../services/bookService');
 const { getErrorMessage } = require('../utils/errorParser');
 
 //Get Catalog Page
-bookRouter.get('/catalog', (req, res) => {
-    res.render('book/catalog');
+bookRouter.get('/catalog', async (req, res) => {
+    const books = await bookService.getAllBooks();
+
+    const hasBooks = books.length > 0;
+
+    res.render('book/catalog', { books, hasBooks });
 });
 
 //Get Create Page
@@ -16,21 +20,21 @@ bookRouter.get('/create', isAuthenticated, (req, res) => {
 
 //Post Create Page
 bookRouter.post('/create', isAuthenticated, async (req, res) => {
-    const book = { 
-        title: req.body.title, 
-        author: req.body.author, 
-        genre: req.body.genre, 
-        stars: req.body.stars, 
-        image: req.body.image, 
+    const book = {
+        title: req.body.title,
+        author: req.body.author,
+        genre: req.body.genre,
+        stars: Number(req.body.stars),
+        image: req.body.image,
         review: req.body.review,
-        owner: req.user._id, 
+        owner: req.user._id,
     }
 
     try {
         await bookService.createBook(book);
     } catch (err) {
         console.log(err);
-        return res.status(400).render('book/create', { error:getErrorMessage(err), book});
+        return res.status(400).render('book/create', { error: getErrorMessage(err), book });
     }
 
     res.redirect('/book/catalog');
--- src/services/bookService.js
+++ src/services/bookService.js
@@ -3,3 +3,16 @@ const Book = require('../models/Book');
 exports.createBook = async (book) => {
     await Book.create(book);
 };
+
+exports.getAllBooks = async ()=>{
+    return await Book.find({}).lean();
+};
+
+//Or like that
+// async function getAll(){
+
+// }
+
+// module.exports = {
+//     getAll
+// }
