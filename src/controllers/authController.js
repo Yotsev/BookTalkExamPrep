f11633b8078ed4a3123abbb6832e365213a48244
Yotsev <b.yotsev@gmail.com>
2023-02-11 08:51:49 +0200

    Added initial structure

---
+++ src/controllers/authController.js
@@ -0,0 +1,44 @@
+const authController = require('express').Router();
+const authService = require('../services/authService');
+const { getErrorMessage } = require('../utils/errorParser');
+
+authController.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
+authController.post('/register', async (req, res) => {
+    const { username, email, password, repeatPassword } = req.body;
+
+    try {
+        const token = await authService.register(username, email, password, repeatPassword);
+
+        res.cookie('auth', token);
+        res.redirect('/');
+    } catch (err) {
+        return res.status(400).render('auth/register', {error: getErrorMessage(err)});
+    }
+});
+
+authController.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+authController.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const token = await authService.login(email, password);
+        
+        res.cookie('auth', token);
+        res.redirect('/');
+    } catch (err) {
+        return res.status(404).render('auth/login', { error: getErrorMessage(err) });
+    }
+});
+
+authController.get('/logout', (req, res) => {
+    res.clearCookie('auth');
+    res.redirect('/');
+});
+
+module.exports = authController;
---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true,'Username is rerequired'],
+        minLength: [5,'Username must be at least 5 characters long'],
+    },
+    email: {
+        type: String,
+        required: [true,'Email is rerequired'],
+        minLength: [10, 'Email must be at least 10 characters long'],
+    },
+    password: {
+        type: String,
+        required: [true,'Password is rerequired'],
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
